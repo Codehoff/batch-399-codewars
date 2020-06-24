549c1eb301034e59ef8ff07bde2788dc68f6bdd1
Enno Kirchhoff <enno.kirchhoff1911@gmail.com>
2020-06-24 17:37:33 +0200

    javascript fixed

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -20,22 +20,33 @@ class MembersController < ApplicationController
     @ruby_score = @member.score
     @rank = @member.rank.split("")[0].to_i
     @kyu = @member.rank
+    
     @percentage = 0
+    @color = nil
     case @rank
     when 8
       @percentage = (@ruby_score * 100 / 20).round(0)
+      @color = "#CCCCCC"
     when 7 
       @percentage = ((@ruby_score - 20) * 100 / (76.0 - 20)).round(0) 
+      @color = "#CCCCCC"
     when 6 
       @percentage = ((@ruby_score - 76) * 100 / (229.0 - 76)).round(0) 
+      @color = "#ECB613"
     when 5 
-      @percentage = ((@ruby_score - 229) * 100 / (643.0 - 229)).round(0) 
+      @percentage = ((@ruby_score - 229) * 100 / (643.0 - 229)).round(0)
+      @color = "#ECB613" 
     when 4 
-      @percentage = ((@ruby_score - 643) * 100 / (1768.0 - 643)).round(0) 
+      @percentage = ((@ruby_score - 643) * 100 / (1768.0 - 643)).round(0)
+      @color = "#3C7EBB"
     when 3 
       @percentage = ((@ruby_score - 1768) * 100 / (4829.0 - 1768)).round(0) 
+      @color = "#3C7EBB"
     when 2
-      @percentage = ((@ruby_score - 4829) * 100 / (13147.0 - 4829)).round(0) 
+      @percentage = ((@ruby_score - 4829) * 100 / (13147.0 - 4829)).round(0)
+      @color = "#866CC7"
+    when 1
+      @color = "#866CC7"
     end
   
   end
